5514f1c50f0cdc478b3ab3a6e508af139857ce83
Rafael Borges <rafaelfborges>
2019-12-28 01:05:12 -0300

    Adicionado testes ao projeto com phpunit

--- exemplo.php
+++ exemplo.php
@@ -3,6 +3,6 @@
     use Red\PhpCep\Search;
 
     $busca = new Search();
-    $resultado = $busca->getAddressFromZipcode('83035170');
+    $resultado = $busca->getAddressFromZipcode('83035090');
 
     print_r($resultado);
---
+++ tests/SearchTest.php
@@ -0,0 +1,52 @@
+<?php
+
+    use PHPUnit\Framework\TestCase;
+    use Red\PhpCep\Search;
+
+    class SearchTest extends TestCase{
+
+        /**
+         * @dataProvider dadosTeste
+         * @param string $input
+         * @param array $esperado
+         */
+        public function testeGetAddressFromZipcodeDefaultUsage(string $input, array $esperado) {
+            $resultado = new Search;
+            $resultado = $resultado->getAddressFromZipcode($input);
+
+            $this->assertEquals($esperado, $resultado);
+        }
+
+        public function dadosTeste(){
+            return [
+                "Endereço Casa do Vô" => [
+                    "83035090",
+                    [
+                        "cep" => "83035-090",
+                        "logradouro" => "Rua Padre João da Veiga Coutinho",
+                        "complemento" => "",
+                        "bairro" => "Cidade Jardim",
+                        "localidade" => "São José dos Pinhais",
+                        "uf" => "PR",
+                        "unidade" => "",
+                        "ibge" => "4125506",
+                        "gia" => ""
+                    ]
+                ],
+                "Endereço de Casa" => [
+                    "83035170",
+                    [
+                        "cep" => "83035-170",
+                        "logradouro" => "Rua Aristides França",
+                        "complemento" => "",
+                        "bairro" => "Cidade Jardim",
+                        "localidade" => "São José dos Pinhais",
+                        "uf" => "PR",
+                        "unidade" => "",
+                        "ibge" => "4125506",
+                        "gia" => ""
+                    ]
+                ],
+            ];
+        }
+    }
